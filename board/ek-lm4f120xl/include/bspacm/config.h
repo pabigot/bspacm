8dc9ee3ca5a7bd030c11e5c7fbba7e94b1f34b69
Peter A. Bigot <pab@pabigot.com>
2014-04-05 11:17:04 -0500

    cleanup: documentation block support for board config.h files

--- board/ek-lm4f120xl/include/bspacm/config.h
+++ board/ek-lm4f120xl/include/bspacm/config.h
@@ -12,11 +12,27 @@
  * <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
 
+/** @file
+ *
+ * @brief Board-specific configuration header for ek-lm4f120xl
+ *
+ * This file may be copied, modified, and installed in an
+ * application-specific include directory to supersede the board
+ * default configuration.  Its contents should be consistent with the
+ * peripheral configuration identified in the @c PERIPH_CONFIG_SRC
+ * make variable.
+ *
+ * @homepage http://github.com/pabigot/bspacm
+ * @publicdomain @cc0_doc
+ */
+
 #ifndef BSPACM_CONFIG_H
 #define BSPACM_CONFIG_H
 
 /** The default UART device on this board */
 #define BSPACM_CONFIG_DEFAULT_UART_HANDLE (&xBSPACMdeviceTM4CperiphUART0)
+
+/* @cond DOXYGEN_EXCLUDE */
 
 #if defined(BSPACM_CONFIG_DEFAULT_UART_TX_BUFFER_SIZE) \
   && ! defined(BSPACM_PERIPH_UART0_TX_BUFFER_SIZE)
@@ -27,5 +43,7 @@
   && ! defined(BSPACM_PERIPH_UART0_RX_BUFFER_SIZE)
 #define BSPACM_PERIPH_UART0_RX_BUFFER_SIZE BSPACM_CONFIG_DEFAULT_UART_RX_BUFFER_SIZE
 #endif /* Copy RX_BUFFER_SIZE */
+
+/* @endcond */
 
 #endif /* BSPACM_CONFIG_H */
