eae2fe8df62358f19db7eebbc647fd6a266c6900
Peter A. Bigot <pab@pabigot.com>
2014-04-14 12:25:45 -0500

    examples/hello: add the traditional example

---
+++ examples/hello/main.c
@@ -0,0 +1,20 @@
+/* BSPACM - bootstrap/hello demonstration application
+ *
+ * Written in 2014 by Peter A. Bigot <http://pabigot.github.io/bspacm/>
+ *
+ * To the extent possible under law, the author(s) have dedicated all
+ * copyright and related and neighboring rights to this software to
+ * the public domain worldwide. This software is distributed without
+ * any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication
+ * along with this software. If not, see
+ * <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+#include <stdio.h>
+
+void main ()
+{
+  printf("Hello, world!");
+}
