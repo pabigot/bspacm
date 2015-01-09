e71937da2138ad4dd5b9df3a1e877ac57240c332
Peter A. Bigot <pab@pabigot.com>
2015-01-09 17:19:08 -0600

    nrf51: implement delegation to soft-device wrappers
    This gets ugly because the wrappers only function when the soft-device
    is enabled and the requested peripheral is restricted by the SD.  This
    means we need to check for a disabled SD and fall back to the non-SD
    implementation in that case.  Coupled with the need to provide the
    non-SD implementation when SD support is not enabled at build time this
    makes for ugly code.
    
    FWIW: Enabling the soft-device appears to take about 1ms and disabling
    it takes about 250us.  The latter time might increase if the device
    was actually configured to do something.

--- device/nrf51/include/bspacm/device.h
+++ device/nrf51/include/bspacm/device.h
@@ -33,6 +33,26 @@
  *
  * @brief Common device header for all nRF51 series devices.
  *
+ * Primarily this contains wrappers that allow code to be generically
+ * capable of running with or without a soft device.
+ *
+ * First, the soft-device wrapper functions should not be used for
+ * peripherals that are not restricted by the soft-device: doing so
+ * can result in hard faults.
+ *
+ * Second, the soft-device wrapper functions are unnecessary if soft
+ * device support is disabled (#BSPACM_NRF_USE_SD).
+ *
+ * Third, even when compiled-in support is present the softdevice
+ * might not enabled at the time the operation is invoked.  In that
+ * situation we need to fall back to the non-SD implementation.
+ *
+ * Because non-SD functionality generally does not indicate errors,
+ * any errors evoked by the soft-device wrappers that can't be
+ * processed immediately causes the application to block.  The
+ * expectation is that these errors are due to mis-use of a restricted
+ * peripheral, and will be discovered during application testing.
+ *
  * @homepage http://github.com/pabigot/bspacm
  * @copyright Copyright 2014, Peter A. Bigot.  Licensed under <a href="http://www.opensource.org/licenses/BSD-3-Clause">BSD-3-Clause</a>
  */
@@ -96,55 +116,123 @@ vBSPACMnrf51NVICsetApplicationPriority (IRQn_Type irqn,
 __STATIC_INLINE void
 vBSPACMnrf51_NVIC_EnableIRQ (IRQn_Type irqn)
 {
+#define NOSD_BODY() do { \
+    NVIC_EnableIRQ(irqn); \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_nvic_EnableIRQ(irqn);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  NVIC_EnableIRQ(irqn);
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware version of NVIC_DisableIRQ */
 __STATIC_INLINE void
 vBSPACMnrf51_NVIC_DisableIRQ (IRQn_Type irqn)
 {
+#define NOSD_BODY() do { \
+    NVIC_DisableIRQ(irqn); \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_nvic_DisableIRQ(irqn);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  NVIC_DisableIRQ(irqn);
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware version of NVIC_GetPendingIRQ */
 __STATIC_INLINE uint32_t
 uiBSPACMnrf51_NVIC_GetPendingIRQ (IRQn_Type irqn)
 {
+  uint32_t pending_irq;
+
+#define NOSD_BODY() do {                    \
+    pending_irq = NVIC_GetPendingIRQ(irqn); \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_nvic_GetPendingIRQ(irqn, &pending_irq);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  return NVIC_GetPendingIRQ(irqn);
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+  return pending_irq;
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware version of NVIC_SetPendingIRQ */
 __STATIC_INLINE void
 vBSPACMnrf51_NVIC_SetPendingIRQ (IRQn_Type irqn)
 {
+#define NOSD_BODY() do {      \
+    NVIC_SetPendingIRQ(irqn); \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_nvic_SetPendingIRQ(irqn);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  NVIC_SetPendingIRQ(irqn);
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware version of NVIC_ClearPendingIRQ */
 __STATIC_INLINE void
 vBSPACMnrf51_NVIC_ClearPendingIRQ (IRQn_Type irqn)
 {
+#define NOSD_BODY() do {         \
+    NVIC_ClearPendingIRQ(irqn);  \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_nvic_ClearPendingIRQ(irqn);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  NVIC_ClearPendingIRQ(irqn);
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware version of NVIC_SetPriority */
@@ -152,33 +240,72 @@ __STATIC_INLINE void
 vBSPACMnrf51_NVIC_SetPriority (IRQn_Type irqn,
                                uint32_t priority)
 {
+#define NOSD_BODY() do {              \
+    NVIC_SetPriority(irqn, priority); \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_nvic_SetPriority(irqn, priority);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  NVIC_SetPriority(irqn, priority);
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware version of NVIC_GetPriority */
 __STATIC_INLINE uint32_t
-uiBSPACMnrf51_NVIC_SetPriority (IRQn_Type irqn)
+uiBSPACMnrf51_NVIC_GetPriority (IRQn_Type irqn)
 {
+#define NOSD_FN() NVIC_GetPriority(irqn)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  nrf_app_irq_priority_t priority;
+  uint32_t ec = sd_nvic_GetPriority(irqn, &priority);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    priority = NOSD_FN();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
+  return priority;
 #else /* BSPACM_NRF_USE_SD */
-  return NVIC_GetPriority(irqn);
+  return NOSD_FN();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_FN
 }
 
 /** Soft-device--aware version of NVIC_SystemReset */
 __STATIC_INLINE void
 vBSPACMnrf51_NVIC_SystemReset (void)
 {
+#define NOSD_BODY() do {  \
+    NVIC_SystemReset();   \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_nvic_SystemReset();
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  return NVIC_SystemReset();
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware configuration of NRF_PPI->CH */
@@ -187,59 +314,133 @@ vBSPACMnrf51_PPI_CH (uint8_t channel_num,
                      const volatile void * evt_endpoint,
                      const volatile void * task_endpoint)
 {
+#define NOSD_BODY() do {                                        \
+    NRF_PPI->CH[channel_num].EEP = (uintptr_t)evt_endpoint;     \
+    NRF_PPI->CH[channel_num].TEP = (uintptr_t)task_endpoint;    \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_ppi_channel_assign(channel_num, evt_endpoint, task_endpoint);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  NRF_PPI->CH[channel_num].EEP = (uintptr_t)evt_endpoint;
-  NRF_PPI->CH[channel_num].TEP = (uintptr_t)task_endpoint;
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware assignment to NRF_PPI->CHENCLR */
 __STATIC_INLINE void
 vBSPACMnrf51_PPI_CHENCLR (uint32_t mask)
 {
+#define NOSD_BODY() do {     \
+    NRF_PPI->CHENCLR = mask; \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_ppi_channel_enable_clr(mask);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  NRF_PPI->CHENCLR = mask;
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware assignment to NRF_PPI->CHENSET */
 __STATIC_INLINE void
 vBSPACMnrf51_PPI_CHENSET (uint32_t mask)
 {
+#define NOSD_BODY() do {     \
+    NRF_PPI->CHENSET = mask; \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t ec = sd_ppi_channel_enable_set(mask);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
 #else /* BSPACM_NRF_USE_SD */
-  NRF_PPI->CHENSET = mask;
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 /** Soft-device--aware read of to NRF_PPI->CHEN */
 __STATIC_INLINE uint32_t
 uiBSPACMnrf51_PPI_CHEN ()
 {
+#define NOSD_FN() NRF_PPI->CHEN
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
+  uint32_t chen;
+  uint32_t ec = sd_ppi_channel_enable_get(&chen);
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    chen = NOSD_FN();
+  } else if (NRF_SUCCESS != ec) {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
+  }
+  return chen;
 #else /* BSPACM_NRF_USE_SD */
-  return NRF_PPI->CHEN;
+  return NOSD_FN();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_FN
 }
 
 /** Soft-device--aware start of HFCLK */
 __STATIC_INLINE void
 vBSPACMnrf51_HFCLKSTART ()
 {
+#define NOSD_BODY() do {                        \
+    NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;         \
+    NRF_CLOCK->TASKS_HFCLKSTART = 1;            \
+    while (! NRF_CLOCK->EVENTS_HFCLKSTARTED) {  \
+    }                                           \
+  } while (0)
+
 #if (BSPACM_NRF_USE_SD - 0)
-  #error not supported
-#else /* BSPACM_NRF_USE_SD */
-  NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
-  NRF_CLOCK->TASKS_HFCLKSTART = 1;
-  while (! NRF_CLOCK->EVENTS_HFCLKSTARTED) {
+  uint32_t ec = sd_clock_hfclk_request();
+
+  if (NRF_ERROR_SOFTDEVICE_NOT_ENABLED == ec) {
+    NOSD_BODY();
+  } else if (NRF_SUCCESS == ec) {
+    uint32_t is_running;
+    do {
+      if (NRF_SUCCESS != sd_clock_hfclk_is_running(&is_running))
+        while (1) {
+          /* deadlock to indicate invalid use */
+        }
+    } while (! is_running);
+  } else {
+    while (1) {
+      /* deadlock to indicate invalid use */
+    }
   }
+#else /* BSPACM_NRF_USE_SD */
+  NOSD_BODY();
 #endif /* BSPACM_NRF_USE_SD */
+
+#undef NOSD_BODY
 }
 
 #endif /* BSPACM_DEVICE_NRF51_H */
